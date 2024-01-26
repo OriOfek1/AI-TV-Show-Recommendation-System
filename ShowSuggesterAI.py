f5bce9b5f87605d61883eb359f72b9c43dc74490
OriOfek1 <oriofek1@gmail.com>
2024-01-26 20:40:15 +0200

    generates a list, need to add actors factor

--- ShowSuggesterAI.py
+++ ShowSuggesterAI.py
@@ -1,7 +1,10 @@
 import logging
+import pickle
 import csv
 from fuzzywuzzy import process
 import numpy as np
+from numpy.linalg import norm
+
 
 
 # Configure logging
@@ -14,7 +17,7 @@ def get_user_input():
     return input("Which TV shows did you love watching? Separate them by a comma. Make sure to enter more than 1 show: ")
 
 def interpret_input_shows_names(user_input):
-    # using fuzzywuzzy to interpret the user input
+    # uses fuzzywuzzy to interpret the user input
 
     input_show_list_raw = user_input.split(',')
 
@@ -41,11 +44,34 @@ def confirm_user_input(shows):
     confirmation = input(f"Just to make sure, do you mean {', '.join(shows)}? (y/n): ")
     return confirmation.lower() == 'y'
 
-def generate_real_tv_show_recommendations():
-    #TODO: Generate TV show recommendations using the embeddings api
-    logging.debug("TV show recommendations generation is pending implementation.\n")
-    return []
+def cosine_similarity(vec_a, vec_b):
+    return np.dot(vec_a, vec_b) / (norm(vec_a) * norm(vec_b))
 
+def generate_real_tv_show_recommendations(user_input_show_list):
+    #add boost for similar actors, genres, etc
+
+
+    with open("tv_show_descriptions_embeddings.pkl", 'rb') as file:
+        all_show_embeddings = pickle.load(file)
+
+    user_input_show_embeddings = {show: all_show_embeddings[show] for show in user_input_show_list if show in all_show_embeddings}
+
+    user_input_vectors = list(user_input_show_embeddings.values())
+    average_vector = np.mean(user_input_vectors, axis=0)
+    logging.debug(f"Average Vector for User Input Shows: {average_vector}\n")
+
+    similarity_scores = {show: cosine_similarity(average_vector, show_embedding) for show, show_embedding in all_show_embeddings.items()}
+    for user_picked_show in user_input_show_list:
+        similarity_scores.pop(user_picked_show, None)
+
+    recommended_shows = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)[:5]
+    logging.debug(f"Recommended Shows: {recommended_shows}\n")
+    print("Top 5 Recommended Shows:\n")
+    for show, score in recommended_shows:
+        print(f"{show}: {score * 100:.0f}%")
+    
+    return recommended_shows
+    
 def generate_made_up_show_input_based(input_shows):
         # TODO: Generate a made-up show based on the input shows
         logging.debug("Generating a made-up show based on input shows is pending implementation.\n")
@@ -88,15 +114,12 @@ def main():
     # Main function to run the TV show recommendation program
     while True:
         user_input = get_user_input()
+        # Interprets user input into shows from the list
         interpreted_shows = interpret_input_shows_names(user_input)
 
         if confirm_user_input(interpreted_shows):
             # Generate recommendations and custom shows
-            recommendations = generate_real_tv_show_recommendations()
-
-            # Display recommendations
-            for recommendation in recommendations:
-                print(f"{recommendation['name']} ({recommendation['score']}%)")
+            recommendations = generate_real_tv_show_recommendations(interpreted_shows)
 
             custom_shows = generate_made_up_shows(interpreted_shows, recommendations)
             break
