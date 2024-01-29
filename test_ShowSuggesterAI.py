fed5b37169e10a002152880339a9a020fd99d0f5
OriOfek1 <oriofek1@gmail.com>
2024-01-29 17:26:17 +0200

    tests added to final verse

--- test_ShowSuggesterAI.py
+++ test_ShowSuggesterAI.py
@@ -6,23 +6,10 @@ def test_interpret_input_shows_names():
     assert ShowSuggesterAI.interpret_input_shows_names("Game of Thrones") == ["Game of Thrones"]
     assert ShowSuggesterAI.interpret_input_shows_names("gem of thrones, lupin, breaking") == ["Game of Thrones", "Lupin", "Breaking Bad"]
     assert ShowSuggesterAI.interpret_input_shows_names("xyz, abc, 123") == []
-# def test_confirm_user_input():
-#     pass
 
-# def test_generate_real_tv_show_recommendations():
-#     pass   
+def test_get_list_of_items_for_shows():
+    genres = set(ShowSuggesterAI.get_list_of_items_for_shows(["The Office", "Fringe"], "Genres"))
+    assert genres == set(['Comedy', 'Drama', 'Mystery', 'Sci-Fi'])
+    actors = set(ShowSuggesterAI.get_list_of_items_for_shows(["Game of Thrones", "Breaking Bad"], "Actors"))
+    assert actors == set(['Bryan Cranston', 'Aaron Paul', 'Peter Dinklage', 'Lena Headey', 'Emilia Clarke', 'Kit Harington','Anna Gunn', 'Betsy Brandt'])
 
-# def test_generate_made_up_show_input_based():
-#     pass
-
-# def test_generate_made_up_show_based_on_recommendations():
-#     pass
-
-# def test_generate_ad_for_a_shows():
-#     pass
-
-# def test_generate_made_up_shows():
-#     pass
-
-# def test_main():
-#     pass
